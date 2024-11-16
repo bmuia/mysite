620ac2ac90d78577e5f342f86154a2f71240ec24
belammuia <belammuia0@gmail.com>
2024-11-16 23:50:06 +0300

    changes

---
+++ src/Components/Data.js
@@ -0,0 +1,177 @@
+const data = [
+    // NON-CURRENT ASSETS
+    {
+        "id": "1",
+        "title": "land",
+        "cost": "30000",
+        "Acc.Depreciation":"",
+        "NBV":"30000"
+    },
+    {
+        "id": "2",
+        "title": "buildings",
+        "cost": "10000",
+        "Acc.Depreciation":"2000",
+        "NBV":"8000"
+    },
+    {
+        "id": "3",
+        "title": "motor vehicle",
+        "cost": "20000",
+        "Acc.Depreciation":"4700",
+        "NBV":"15300"
+    },
+    // CURRENT ASSETS
+    {
+        "id": "4",
+        "title": "inventory stock",
+        "cost": "",
+        "Acc.Depreciation":"800",
+        "NBV":""
+    },
+    {
+        "id": "5",
+        "title": "trade debtors",
+        "cost": "12000",
+        "Acc.Depreciation":"",
+        "NBV":""
+    },
+    {
+        "id": "6",
+        "title": "less provision for bad debts",
+        "cost": "",
+        "Acc.Depreciation":"11000",
+        "NBV":""
+    },
+    {
+        "id": "7",
+        "title": "bank(equity bank blce)",
+        "cost": "",
+        "Acc.Depreciation":"8000",
+        "NBV":""
+    },
+    {
+        "id": "8",
+        "title": "cash",
+        "cost": "",
+        "Acc.Depreciation":"5000",
+        "NBV":""
+    },
+    {
+        "id": "9",
+        "title": "prepaid salaries",
+        "cost": "",
+        "Acc.Depreciation":"600",
+        "NBV":""
+    },
+    {
+        "id": "10",
+        "title": "T.C.A",
+        "cost": "",
+        "Acc.Depreciation":"",
+        "NBV":"25400"
+    },
+    {
+        "id": "11",
+        "title": "",
+        "cost": "",
+        "Acc.Depreciation":"",
+        "NBV":"78700"
+    },
+    // EQUITY AND LIABILITIES
+
+    {
+        "id": "12",
+        "title": "capital and reserves",
+        "cost": "",
+        "Acc.Depreciation":"",
+        "NBV":""
+    },
+    {
+        "id": "13",
+        "title": "capital",
+        "cost": "",
+        "Acc.Depreciation":"",
+        "NBV":"61300"
+    },
+    {
+        "id": "14",
+        "title": "less : loss",
+        "cost": "",
+        "Acc.Depreciation":"",
+        "NBV":"-25100"
+    },
+    {
+        "id": "15",
+        "title": "",
+        "cost": "",
+        "Acc.Depreciation":"",
+        "NBV":"36200"
+    },
+    {
+        "id": "16",
+        "title": "less : drawings",
+        "cost": "",
+        "Acc.Depreciation":"",
+        "NBV":"3000"
+    },
+    {
+        "id": "17",
+        "title": "total capital and reserves",
+        "cost": "",
+        "Acc.Depreciation":"",
+        "NBV":"33200"
+    } ,
+    // LIABILITIES
+    {
+        "id": "18",
+        "title": "Non current liabilities",
+        "cost": "",
+        "Acc.Depreciation":"",
+        "NBV":""
+    },
+    {
+        "id": "19",
+        "title": "DBT bank loan",
+        "cost": "",
+        "Acc.Depreciation":"",
+        "NBV":"30000"
+    },
+    {
+        "id": "20",
+        "title": "current liabilities",
+        "cost": "",
+        "Acc.Depreciation":"",
+        "NBV":""
+    },
+    {
+        "id": "21",
+        "title": "creditors",
+        "cost": "",
+        "Acc.Depreciation":"15000",
+        "NBV":""
+    },
+    {
+        "id": "22",
+        "title": "advertising due",
+        "cost": "",
+        "Acc.Depreciation":"500",
+        "NBV":""
+    },
+    {
+        "id": "23",
+        "title": "total current liabilities",
+        "cost": "",
+        "Acc.Depreciation":"",
+        "NBV":"33200"
+    },
+    {
+        "id": "24",
+        "title": "total equity and liabilitiess",
+        "cost": "",
+        "Acc.Depreciation":"",
+        "NBV":"78700"
+    }
+
+]
+export default data;
